0e6c75af147bdd77f67b8efae9c958cab9201b1c
RuBrock <r.brocksilva@yahoo.com.br>
2022-02-16 00:32:16 -0300

    Edit env to access heroku var

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use('/', (request, response) => {
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.CONNECTION_URL, 
+  .connect(ENV['CONNECTION_URL'], 
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => 
